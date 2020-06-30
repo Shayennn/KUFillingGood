eb7f996605d53e1c46dc6b3d8d6a6b46df57e664
Shayennn <me@shayennn.com>
2020-06-30 18:38:03 +0700

    Show currentCredit

--- public/result.js
+++ public/result.js
@@ -112,6 +112,7 @@ function prepareDownload() {
 }
 
 function renderModal(){
+    document.getElementById('currentCredit').innerText = Sess['currentCredit']
     const tb = document.getElementById('sublist')
     tb.innerHTML = ''
     var FixedSubjectInfo = Sess['FixedSubjectInfo'].sort((a, b)=>{
